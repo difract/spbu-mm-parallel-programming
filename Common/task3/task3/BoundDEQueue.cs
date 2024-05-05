b7d8bf77b8a305149561770252e7efd3082cc892
Vladislav <111283248+difract@users.noreply.github.com>
2024-05-05 17:10:36 +0300

    Загружено решение задачи 3

---
+++ Common/task3/task3/BoundDEQueue.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace task3
+{
+    public class BDEQueue : IDEQueue<IMyTask>
+    {
+        private List<IMyTask> Queue;
+        public object Locker;
+        public BDEQueue()
+        {
+            Queue = new List<IMyTask>();
+            Locker = new object();
+        }
+
+        public int Count()
+        {
+            lock (Locker)
+            {
+                return Queue.Count;
+            }
+        }
+
+        public bool IsEmpty()
+        {
+            lock (Locker)
+            {
+                return Queue.Count == 0;
+            }
+        }
+
+        public void Enqueue(IMyTask value)
+        {
+            lock (Locker)
+            {
+                Queue.Insert(0, value);
+            }
+        }
+
+        public IMyTask PopBottom()
+        {
+            lock (Locker)
+            {
+                if (Queue.Count < 1 || Queue.Last() == null)
+                    return null;
+                IMyTask task = Queue.Last();
+                Queue.RemoveAt(Queue.Count - 1);
+                return task;
+            }
+        }
+
+        public IMyTask PopTop()
+        {
+            lock (Locker)
+            {
+                if (Queue.Count < 1 || Queue.First() == null)
+                    return null;
+                IMyTask task = Queue.First();
+                Queue.RemoveAt(0);
+                return task;
+            }
+        }
+    }
+}
---
+++ Common/task3/task3/MyTask.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+namespace task3
+{
+    public class MyTask<T> : IMyTask
+    {
+        private Func<T> Job;
+        private ThreadPool MyPool;
+        public bool IsCompleted;
+        public T Result;
+
+        public MyTask(Func<T> newJob)
+        {
+            IsCompleted = true;
+            Result = default(T);
+            Job = newJob;
+        }
+
+        public void SetPool(ThreadPool pool)
+        {
+            MyPool = pool;
+        }
+
+        public void Start()
+        {
+            IsCompleted = false;
+            try
+            {
+                Result = Job();
+                IsCompleted = true;
+            }
+            catch (Exception E)
+            {
+                IsCompleted = false;
+                throw new AggregateException(E);
+            }
+        }
+
+        public T GetResult()
+        {
+            return Result;
+        }
+        
+        public MyTask<TNew> ContinueWith<TNew>(MyTask<T> oldTask, Func<T, TNew> func)
+        {
+            {
+                MyTask<TNew> newTask = new MyTask<TNew>(() => func(oldTask.GetResult()));
+
+                MyPool.Enqueue(newTask);
+                /*
+                Action doContinuation = () =>
+                {
+                    newTask.Func = () => func(Result);
+                    MyPool.Enqueue(newTask);
+                };
+                Action doFail = () =>
+                {
+                    newTask.exceptions.AddRange(exceptions);
+                    newTask.exceptions.Add(new ParentTaskFailException(this));
+                    lock (newTask.Locker)
+                    {
+                        newTask.IsFailed = true;
+                    }
+                    newTask.OnFailed?.Invoke();
+                };
+
+                lock (Locker)
+                {
+                    if (IsCompleted)
+                        doContinuation();
+                    else if (IsFailed)
+                        doFail();
+                    else
+                    {
+                        OnCompleted += doContinuation;
+                        OnFailed += doFail;
+                    }
+                }
+                */
+                return newTask;
+            }
+        }
+    }
+}
