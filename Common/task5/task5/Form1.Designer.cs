2e20872d45d1f1a4fec6659e52ac53b5e3c9fdc5
Vladislav <111283248+difract@users.noreply.github.com>
2024-05-23 17:57:54 +0300

    Added task5 solution

---
+++ Common/task5/task5/Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿
+namespace task5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.HideSelection = false;
+            this.richTextBox1.Location = new System.Drawing.Point(68, 34);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.Vertical;
+            this.richTextBox1.Size = new System.Drawing.Size(390, 234);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(68, 289);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(390, 22);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(68, 467);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(169, 50);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Connect";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(296, 467);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(162, 50);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Exit";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Enabled = false;
+            this.button3.Location = new System.Drawing.Point(68, 317);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(169, 50);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Send";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(68, 413);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(390, 22);
+            this.textBox2.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(65, 271);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(177, 17);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Сообщение для отправки";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(68, 389);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(268, 17);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Введите свой никнейм для соединения";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.Color.Red;
+            this.label3.Location = new System.Drawing.Point(68, 438);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(270, 17);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Без никнейма подлючение невозможно";
+            this.label3.Visible = false;
+            // 
+            // button4
+            // 
+            this.button4.Enabled = false;
+            this.button4.Location = new System.Drawing.Point(289, 317);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(169, 50);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "Clear text field";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(514, 545);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "P2P Chat";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ Common/task5/task5/Form1.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+using System.Net.Sockets;
+using System.Net;
+
+namespace task5
+{
+    public partial class Form1 : Form
+    {
+        private delegate void SafeCallDelegate(string text);
+        private Socket McastSendSocket;
+        private Socket McastRecvSocket;
+        private IPEndPoint RemoteEndPoint;
+        private CancellationTokenSource TokenSource;
+        private Thread Listener;
+        private string UserName;
+        public Form1()
+        {
+            InitializeComponent();
+            RemoteEndPoint = new IPEndPoint(new IPAddress(new byte[] { 255, 255, 255, 255 }), 8888);
+            TokenSource = new CancellationTokenSource();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox2.Text != "")
+            {
+                label3.Visible = false;
+                UserName = textBox2.Text;
+            }
+            else
+            {
+                label3.Visible = true;
+                return;
+            }
+            try
+            {
+                McastSendSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+                McastSendSocket.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.Broadcast, true);
+                McastSendSocket.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true);
+            }
+            catch (SocketException se)
+            {
+                richTextBox1.AppendText(se.Message + '\n');
+                return;
+            }
+            Send("User " + UserName + " has connected", true);
+            Listener = new Thread(() => Listen(TokenSource.Token));
+            Listener.Start();
+            textBox1.Enabled = true;
+            button3.Enabled = true;
+            button1.Enabled = false;
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "")
+            {
+                Send(textBox1.Text, false);
+                textBox1.Text = "";
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text = "";
+        }
+
+        private void WriteTextSafe(string text)     //ThreadSafe richTextBox usage
+        {
+            if (textBox1.InvokeRequired)
+            {
+                var del = new SafeCallDelegate(WriteTextSafe);
+                richTextBox1.Invoke(del, new object[] { text });
+            }
+            else
+            {
+                richTextBox1.AppendText(text);
+            }
+        }
+
+        private void Listen(CancellationToken token)        //Cancellable Listener Thread
+        {
+            try
+            {
+                McastRecvSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+                McastRecvSocket.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.Broadcast, true);
+                McastRecvSocket.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true);
+                var broadcastEndpoint = new IPEndPoint(IPAddress.Any, 8888);
+                McastRecvSocket.Bind(broadcastEndpoint);
+            }
+            catch (SocketException se)
+            {
+                WriteTextSafe(se.Message + "FromListener\n");
+                return;
+            }
+            while (!token.IsCancellationRequested)
+            {
+                if (McastRecvSocket.Available != 0)
+                {
+                    try
+                    {
+                        byte[] b = new byte[1024];
+                        McastRecvSocket.Receive(b);
+                        WriteTextSafe(Encoding.UTF8.GetString(b, 0, b.Length) + '\n');
+                    }
+                    catch (SocketException se)
+                    {
+                        WriteTextSafe(se.Message + "FromListener\n");
+                        return;
+                    }
+                    catch(ObjectDisposedException)
+                    {
+                        return;
+                    }
+                }
+                Thread.Yield();
+            }
+        }
+        private void Send(string message, bool system)       //Send in main thread
+        {
+            try
+            {
+                byte[] byteMessage;
+                if (system)
+                    byteMessage = Encoding.UTF8.GetBytes(message + '\n');
+                else
+                    byteMessage = Encoding.UTF8.GetBytes(UserName + ": " + message + '\n');
+                McastSendSocket.SendTo(byteMessage, RemoteEndPoint);
+            }
+            catch (SocketException se)
+            {
+                WriteTextSafe(se.Message + "FromSend\n");
+                return;
+            }
+        }
+
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            Visible = false;
+            if (McastRecvSocket != null)
+                McastRecvSocket.Shutdown(SocketShutdown.Both);
+            if (Listener != null)
+            {
+                TokenSource.Cancel();
+                Listener.Join();
+            }
+            if (McastRecvSocket != null)
+                McastRecvSocket.Close();
+            if (McastSendSocket != null)
+            {
+                Send("User " + UserName + " has disconnected", true);
+                McastSendSocket.Shutdown(SocketShutdown.Both);
+                McastSendSocket.Close();
+            }
+            TokenSource.Dispose();
+            base.OnFormClosing(e);
+        }
+    }
+}
